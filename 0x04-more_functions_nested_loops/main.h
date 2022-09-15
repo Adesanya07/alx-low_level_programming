b97202472f21a1052afac5be8a7ca9b1478d2d53
Gabriel Abayomi Adesanya <110470758+Adesanya07@users.noreply.github.com>
2022-09-15 12:16:10 +0000

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,19 +1,17 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int isPrime(long int n);
-long int biggestFactor(long int a);
 void print_number(int n);
-int numDigits(int n);
 
+#endif
