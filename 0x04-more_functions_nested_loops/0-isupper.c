4ff0d43b404fca7fe2918b6da8e8698d23d00f32
Gabriel Abayomi Adesanya <110470758+Adesanya07@users.noreply.github.com>
2022-09-15 12:04:30 +0000

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _isdigit - input
- *
- * Description: define if a character is a number
- * @c: charater
- * Return: 1 yes 0 no
- */
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
 
-int _isdigit(int c)
+int _isupper(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
