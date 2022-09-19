65fcb901d6027ccc57c4bfb39c37bb3ddbedb91d
Gabriel Abayomi Adesanya <110470758+Adesanya07@users.noreply.github.com>
2022-09-19 13:17:22 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,26 +1,32 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "main.h"
 
-/**
- * main - entry point
- *
- * Return: generated password
+/** 
+ *main- genrate password
+ *Return:nothing
  */
-
 int main(void)
 {
-	char c;
-	int x;
+srand((unsigned int)time(NULL));
+char pass[13];
+int i;
 
-	srand(time(0));
-	while (x <= 2645)
-	{
-		c = rand() % 128;
-		x += c;
-		putchar(c);
-	}
-	putchar(2772 - x);
 
-	return (0);
+for (i = 0; i < 4; i++)
+{
+
+pass[3 * i] = '0' + (rand() % 10);
+char capLetter = 'A' + (rand() % 26);
+
+pass[(3 * i) + 1] = capLetter;
+char letter = 'a' + (rand() % 26);
+
+pass[(3 * i) + 2] = letter;
+}
+pass[3 * i] = '\0';
+printf("generated password : %s\n\n", pass);
+
+printf("\n\n");
+return (0);
+}
