acbb8cd07fc2c8e3cc97f38b71e2f753bc76860e
Adesanya07 <Gadesanya22@gmail.com>
2022-09-20 03:36:42 -0700

    workabeg

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  *  0-reset_to_98.c - a function to update a reference of a pointer
  * @n : pointer to n to be updated
  */
-void void reset_to_98(int *n);
+void reset_to_98(int *n);
 {
 	*n = 98;
 }
