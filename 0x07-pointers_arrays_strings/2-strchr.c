b5629d09853c8dc2b084d1be24e0a8e3c8c0bd7b
Gabriel Abayomi Adesanya <110470758+Adesanya07@users.noreply.github.com>
2022-09-26 16:23:16 +0000

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+char *_strchr(char *s, char c)
+{
+    char *ptr;
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == c)
+        {
+            return (s + i); 
+        }
+    }
+    return ('\0');
+    
+}
+
+int main ()
+{
+
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
+
+}
+Footer
+© 2022 GitHub, Inc.
